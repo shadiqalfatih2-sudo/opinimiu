c7602bde8d1515ab34f2da71ed7b1a94d090110a
shadiqalfatih2-sudo <shadiqalfatih2@gmail.com>
2026-09-06 08:30:25 +0800

    feat: refresh Opinimiu wordmark for new visual system

--- components/Brand.tsx
+++ components/Brand.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 export default function Brand() {
   return (
-    <Link className="brand brand-official" href="/" aria-label="Opinimiu Celebest - Beranda">
-      <img src="/opinimiu-logo.webp" alt="Opinimiu Celebest" />
+    <Link className="brand brand-official ub-brand" href="/" aria-label="Opinimiu - Beranda">
+      <span className="ub-brand-word">Opini<span>miu</span></span>
+      <small>SUARA JERNIH UNTUK SULAWESI TENGAH</small>
     </Link>
   );
 }
